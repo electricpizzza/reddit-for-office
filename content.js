914f865fccbf2ddf66563d79be558a23b8ac3e2d
unknown <zakariae.dinar@gmail.com>
2022-02-07 17:36:45 +0100

    Adding some plugin's script & test

---
+++ content.js
@@ -0,0 +1,5 @@
+const subTitle = document.getElementsByClassName("MSTY2ZpsdupobywLEfx9u");
+const banner = document.getElementsByClassName("_2L5G9B5yaoqW3IegiYN-FL");
+
+$(subTitle).hide();
+$(banner).hide();
