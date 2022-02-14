dfc5a3372eab4ef7271cbcfb94db50c47ea0423a
unknown <zakariae.dinar@gmail.com>
2022-02-14 10:26:56 +0100

    Testing

--- popup.js
+++ popup.js
@@ -18,8 +18,14 @@ function togglePageState() {
     chrome.storage.sync.set({ hiden: !hiden });
     $(".MSTY2ZpsdupobywLEfx9u").toggle(hiden);
     $("._2L5G9B5yaoqW3IegiYN-FL").toggle(hiden);
-    $(
-      "._10K5i7NW6qcm-UoCtpB3aK _3LwUIE7yX7CZQKmD2L87vf._1F02c6Yw0dfhdWwl99UrYn._1fiOgAxLiYfEU41C1NOX9B._1IKtbRloF_LV1hPqMzP3MC"
-    ).click();
+
+    /**
+     *  To Make posts compact
+     * 
+     *   
+      $(
+        "._10K5i7NW6qcm-UoCtpB3aK._3LwUIE7yX7CZQKmD2L87vf._1F02c6Yw0dfhdWwl99UrYn ._1oYEKCssGFjqxQ9jJMNj5G"
+      ).click();
+    */
   });
 }
